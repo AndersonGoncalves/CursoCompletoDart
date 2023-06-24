1e7f8d488788cd2a4d3d2f95b42eb9abfd05d08b
Anderson Gonçalves <anndersonn.gonncalves@gmail.com>
2023-06-24 13:07:55 -0300

    Strings

--- 01_fundamentos/3_variaveis_2.dart
+++ 01_fundamentos/3_variaveis_2.dart
@@ -7,7 +7,34 @@ main() {
   print(n1.runtimeType);
   print(n2.runtimeType);
   print(texto.runtimeType);
-  
+
   print(n1 is double);
-  print(!(n2 is String));  
+  print(!(n2 is String));
+
+  String nome = 'Fernando';
+  String nomeCompleto = '\t Fernando Martins';
+  print(nome.toLowerCase()); //fernando
+  print(nome.toUpperCase()); //FERNANDO
+  print(nomeCompleto.trim()); //Fernando Martins
+  print(nomeCompleto.split(' ')); //[ , Fernando, Martins]
+  print(nome.split('')); //[F, e, r, n, a, n, d, o]
+  print(nome.substring(3, 8)); //nando
+  print(nome.startsWith('F')); //true
+  print(nome.startsWith('nando', 3)); //true
+  print(nome.replaceAll('a', 'o')); //Fernondo
+  print(nome.replaceFirst('Fer', '')); //nando
+  print(nome.replaceFirst('o', 'a', 7)); //Fernanda
+  print(nome.replaceRange(3, 5, '')); //Ferndo
+  print(nomeCompleto.trim().split(' ')); //[Fernando, Martins]
+  print(nome.contains('f')); //false
+  print(nome.contains('F')); //true
+  print(nome.contains('e', 1)); //true
+  print(nome.length); //8 - retorna o comprimento do array
+  print(nome.endsWith('o')); //true
+  print(nome.compareTo('Fernando')); //0 se for igual
+  print(nome.compareTo('Fer')); //1 se possuir
+  print(nome.compareTo('Anderson')); //-1 se diferente
+  print(' '.isEmpty); //false
+  print('1'.padLeft(2, '0')); //01
+  print(nome.indexOf('nando')); //3 - retorna o indece de acordo com referencia
 }
